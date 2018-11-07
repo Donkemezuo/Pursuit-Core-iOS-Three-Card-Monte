451b30988cdc7e5ffcf51a571d24aab1c08046c8
Donkemezuo <raymonddonkemezuo.org>
2018-11-07 13:39:51 -0500

    Final project

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -1,10 +1,4 @@
-//
-//  ViewController.swift
-//  ThreeCardMonte
-//
-//  Created by Alex Paul on 11/5/18.
-//  Copyright © 2018 Pursuit. All rights reserved.
-//
+
 
 import UIKit
 
@@ -18,15 +12,80 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var Card2: UIButton!
     
-    var myCards:[UIImage] = []
+    @IBOutlet weak var displayLabel: UILabel!
     
+    // Action
+    
+    @IBAction func shuffledCard(_ sender: UIButton){
+    
+        var cardLogos: [UIImage] = [
+        UIImage(named:"kingCard")!, UIImage(named:"threeCard")!, UIImage(named: "threeCard")!]
+        
+    cardLogos = cardLogos.shuffled()
+        
+        switch sender.tag {
+        case 0:
+            if cardLogos[0] == UIImage(named: "kingCard")! {
+                Card0.setImage(UIImage.init(named: "kingCard"), for: .normal)
+                displayLabel.text = "You win 🏆"
+            } else {
+                if cardLogos[0] != UIImage(named: "kingCard")! {
+               Card0.setImage(UIImage.init(named: "threeCard"), for: .normal)
+                    displayLabel.text = "Sorry you lost 💀"
+            }
+            }
+            Card0.isEnabled = false
+            Card1.isEnabled = false
+            Card2.isEnabled = false
+        case 1:
+            if cardLogos[1] == UIImage(named: "kingCard")! {
+                Card1.setImage(UIImage.init(named: "kingCard"), for: .normal)
+                displayLabel.text = "You win 🏆"
+            } else {
+                if cardLogos[1] != UIImage(named: "kingCard"){
+                Card1.setImage(UIImage.init(named: "threeCard"), for: .normal)
+                displayLabel.text = "Sorry you lost 💀"
+            }
+            }
+            Card0.isEnabled = false
+            Card1.isEnabled = false
+            Card2.isEnabled = false
+        case 2:
+            if cardLogos[2] == UIImage(named: "kingCard")! {
+                Card2.setImage(UIImage.init(named: "kingCard"), for: .normal)
+                displayLabel.text = "You win 🏆"
+            } else {
+                if cardLogos[2] != UIImage(named: "kingCard"){
+                Card2.setImage(UIImage.init(named: "threeCard"), for: .normal)
+                displayLabel.text = "Sorry you lost 💀"
+            }
+            }
+            Card0.isEnabled = false
+            Card1.isEnabled = false
+            Card2.isEnabled = false
+            
+        default:
+            print("Invalid Card")
+        }
+        Card0.isEnabled = false
+        Card1.isEnabled = false
+        Card2.isEnabled = false
+    }
+   
     override func viewDidLoad() {
     super.viewDidLoad()
    
-        myCards = []
-  }
-
+}
+    
+    @IBAction func NewGame(_ sender: Any){
+        displayLabel.text = "Pick a Card"
+        Card0.isEnabled = true
+        Card1.isEnabled = true
+        Card2.isEnabled = true
+        
+        Card0.setImage(UIImage.init(named: "cardBackRed"), for: .normal)
+          Card1.setImage(UIImage.init(named: "cardBackRed"), for: .normal)
+          Card2.setImage(UIImage.init(named: "cardBackRed"), for: .normal)
+    }
 
 }
-
-
